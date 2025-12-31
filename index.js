95afa8bd0e76cf1c100400369ac88b0b05fb3c86
Amar Rakhpasre <154906069+amar-rakhpasre@users.noreply.github.com>
2025-12-31 12:35:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,19 +57,6 @@ app.listen(PORT, '0.0.0.0', () => {
 });
 
 
-app.get('/health', (req, res) => {
-  const data = {
-        status: 'UP',
-        env: process.env.NODE_ENV || 'unknown',
-        port: process.env.PORT,
-        date: new Date()
-   };
-    
-  res.status(200).send(data);
-
-});
-
-
 router.get('/health', (req, res) => {
   const data = {
     message: 'UP',
